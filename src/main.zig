e000b53e370373d34897a6f74f6fdbbabbaca7e7
vt <me@vtcodes.dev>
2025-05-08 14:21:54 +0200

    refactor: enhance git command execution and staging process
    - introduce `ExecResult` struct for command execution results
    - update `exec()` function to return `ExecResult` instead of a boolean
    - refactor `git` methods to utilize new `exec()` structure
    - improve error handling and logging

--- src/main.zig
+++ src/main.zig
@@ -2,30 +2,57 @@ const std = @import("std");
 const root = @import("root.zig");
 const process = std.process;
 const Allocator = std.mem.Allocator;
+const log = std.log;
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var args = try process.argsWithAllocator(allocator);
-    defer args.deinit();
+    try generateCommit(allocator);
+}
 
-    // Skip the program name
-    _ = args.next();
+fn generateCommit(allocator: Allocator) !void {
+    const git = root.Git.init(allocator);
 
-    const git_check = [_][]const u8{ "git", "rev-parse", "--is-inside-work-tree" };
-    if (!try root.exec(&git_check, allocator, .IgnoreOutput)) {
-        return error.NotGitRepository;
+    if (!try git.isGitRepo()) {
+        std.log.err("Not a Git repository.", .{});
+        return;
     }
 
-    const git_status = [_][]const u8{ "git", "status", "--porcelain" };
-    if (!try root.exec(&git_status, allocator, .CaptureOutput)) {
-        return error.CommandFailed;
+    if (!try git.hasChanges()) {
+        std.log.info("No changes to commit.", .{});
+        return;
     }
 
-    const git_add = [_][]const u8{ "git", "add", "." };
-    if (!try root.exec(&git_add, allocator, .CaptureOutput)) {
-        return error.CommandFailed;
+    const files_to_stage = try git.filesToBeStaged();
+    if (files_to_stage == null) {
+        std.log.info("No changes to commit.", .{});
+        return;
     }
+    defer allocator.free(files_to_stage.?);
+
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    try stdout.print("Files to be staged:\n {s}\n", .{files_to_stage.?});
+    try stdout.print("Stage these files? (y/n): ", .{});
+
+    const confirm = try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 10)
+        orelse return error.InputFailed;
+    defer allocator.free(confirm);
+
+    const trimmed = std.mem.trim(u8, confirm, " \n");
+    if (!std.mem.eql(u8, trimmed, "y") and !std.mem.eql(u8, trimmed, "yes")) {
+        try stdout.print("Staging aborted.\n", .{});
+        return;
+    }
+
+    const curr_branch = try git.currentBranch();
+    if (std.mem.eql(u8, curr_branch, "main") or std.mem.eql(u8, curr_branch, "master")) {
+        try git.createBranch("wip");
+    }
+
+    try git.stageFiles();
+    try stdout.print("Files staged successfully.\n", .{});
 }
