a3ce3ac8d3b3cde1a052e7ec908e5b68c248faac
vt <me@vtcodes.dev>
2025-05-10 12:02:31 +0200

    refactor(openai): refactor OpenAI module structure

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const Git = @import("Git.zig");
-const llm = @import("llm.zig");
+const OpenAI = @import("OpenAI.zig");
 const prompt = @import("prompt.zig");
 const builtin = @import("builtin");
 const Allocator = std.mem.Allocator;
@@ -12,7 +12,7 @@ const CommitMessage = struct {
     body: []const u8,
     footer: []const u8,
 
-    pub fn deinit(self: CommitMessage, allocator: Allocator) void {
+    fn deinit(self: CommitMessage, allocator: Allocator) void {
         allocator.free(self.subject);
         allocator.free(self.body);
         allocator.free(self.footer);
@@ -25,9 +25,9 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     if (comptime builtin.mode == .Debug) {
-        std.log.warn("This is a debug build. Performance will be very poor.", .{});
-        std.log.warn("You should only use a debug build for developing Zenpai.", .{});
-        std.log.warn("Otherwise, please rebuild in a release mode.\n", .{});
+        log.warn("This is a debug build. Performance will be very poor.", .{});
+        log.warn("You should only use a debug build for developing Zenpai.", .{});
+        log.warn("Otherwise, please rebuild in a release mode.\n", .{});
     }
 
     try generateCommit(allocator);
@@ -37,18 +37,18 @@ fn generateCommit(allocator: Allocator) !void {
     var git: Git = .init(allocator);
 
     if (!try git.isGitRepo()) {
-        std.log.err("Not a Git repository.", .{});
+        log.err("Not a Git repository.", .{});
         return;
     }
 
     if (!try git.hasChanges()) {
-        std.log.info("No changes to commit.", .{});
+        log.info("No changes to commit.", .{});
         return;
     }
 
     const files_to_stage = try git.filesToBeStaged();
     if (files_to_stage == null) {
-        std.log.info("No changes to commit.", .{});
+        log.info("No changes to commit.", .{});
         return;
     }
     defer allocator.free(files_to_stage.?);
@@ -81,7 +81,7 @@ fn generateCommit(allocator: Allocator) !void {
     defer allocator.free(diff);
 
     const commitMsg = try generateCommitMsg(allocator, diff) orelse {
-        std.log.info("No commit message generated.", .{});
+        log.info("No commit message generated.", .{});
         return;
     };
     defer commitMsg.deinit(allocator);
@@ -91,17 +91,17 @@ fn generateCommit(allocator: Allocator) !void {
 }
 
 fn generateCommitMsg(allocator: Allocator, diff: []const u8) !?CommitMessage {
-    var openai = try llm.Client.init(allocator, null);
+    var openai: OpenAI = try .init(allocator, null);
     defer openai.deinit();
 
     const user_prompt = try std.fmt.allocPrint(allocator, "Here is the diff:\n\n{s}", .{diff});
     defer allocator.free(user_prompt);
 
-    var messages = std.ArrayList(llm.Message).init(allocator);
-    try messages.append(llm.Message.system(prompt.system));
-    try messages.append(llm.Message.user(user_prompt));
+    var messages = std.ArrayList(OpenAI.Message).init(allocator);
+    try messages.append(OpenAI.Message.system(prompt.system));
+    try messages.append(OpenAI.Message.user(user_prompt));
 
-    const payload = llm.ChatPayload{
+    const payload = OpenAI.ChatPayload{
         .model = "gpt-3.5-turbo",
         .messages = messages.items,
         .max_tokens = 1024,
@@ -119,7 +119,7 @@ fn generateCommitMsg(allocator: Allocator, diff: []const u8) !?CommitMessage {
     const parsed = std.json.parseFromSliceLeaky(CommitMessage, allocator, msg_content, .{
         .ignore_unknown_fields = true,
     }) catch |err| {
-        std.log.err("Failed to parse JSON: {}", .{err});
+        log.err("Failed to parse JSON: {}", .{err});
         return null;
     };
 
